52d0ff8139d9fe29c7f7fc2a071e5d0dfed5f7c6
walaniam <mariusz.walania@gmail.com>
2022-03-04 09:53:54 +0100

    Kafka bootstrap servers configuration

--- src/main/java/walaniam/stock/kafka/producer/config/KafkaTopicConfig.java
+++ src/main/java/walaniam/stock/kafka/producer/config/KafkaTopicConfig.java
@@ -1,36 +1,18 @@
 package walaniam.stock.kafka.producer.config;
 
-import com.google.common.collect.ImmutableMap;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
-
-    @Value("${spring.kafka.producer.bootstrap-servers}")
-    private String bootstrapAddress;
 
     @Value("${stock.kafka.topic}")
     private String topic;
 
     @Value("${stock.kafka.partitions}")
     private int partitions;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = ImmutableMap.of(
-                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress
-        );
-        KafkaAdmin admin = new KafkaAdmin(configs);
-        admin.setAutoCreate(false);
-        return admin;
-    }
 
     @Bean
     public NewTopic stockDataTopic() {
